e313f3ec0d8b4cbbb82d90a505c7185d2ab5b910
Harper Klotz <harperklotz@Harpers-MacBook-Air.local>
2022-11-16 14:43:04 -0500

    fixing badge url

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,11 +1,21 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== 'no license') {
+    return `![badge](https://img.shields.io/badge/license-${license}-blue)`
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  
+  if (license !== 'no license') {
+    return `[${license}](https://choosealicense.com/licenses/${license})`
+  } else {
+    return '';
+  }
 }
 
 // TODO: Create a function that returns the license section of README
@@ -54,9 +64,6 @@ function generateMarkdown(data) {
   Have any questions? You can reach me here:
   GitHub: https://github.com/${data.github}
   Email: ${data.email}
-
-  ## Credits
-  ${data.name}
 `;
 }
 
